070fe9fab64e07a9c67f56d85c8ba330b27626d8
琉夏 吉田 <230395@jc-21.jp>
2025-01-14 16:43:05 +0900

    OutLineのシェーダー作ったけど動作しないからいろいろ戻した

---
+++ OutLineShader.hlsl
@@ -0,0 +1,68 @@
+//───────────────────────────────────────
+// テクスチャ＆サンプラーデータのグローバル変数定義
+//───────────────────────────────────────
+Texture2D g_texture : register(t0); //テクスチャー
+SamplerState g_sampler : register(s0); //サンプラー
+
+Texture2D g_toon_texture : register(t1); //テクスチャー
+
+//───────────────────────────────────────
+// コンスタントバッファ
+// DirectX 側から送信されてくる、ポリゴン頂点以外の諸情報の定義
+//───────────────────────────────────────
+cbuffer global : register(b0)
+{
+    //変換行列、視点、光源
+    float4x4 matWVP; // ワールド・ビュー・プロジェクションの合成行列
+    float4x4 matW; //ワールド変換マトリクス
+    float4x4 matNormal; //法線をワールド座標に対応させる行列＝回転＊スケール
+    float4 diffuseColor; // ディフューズカラー（マテリアルの色）
+    float4 factor; //ディフューズファクター(diffuseFactor)
+    float4 ambientColor;
+    float4 specularColor;
+    float4 shininess;
+    
+    bool isTextured; // テクスチャ貼ってあるかどうか
+};
+
+cbuffer gStage : register(b1)
+{
+    float4 lightPosition;
+    float4 eyePosition;
+}
+
+//───────────────────────────────────────
+// 頂点シェーダー出力＆ピクセルシェーダー入力データ構造体
+//───────────────────────────────────────
+struct VS_OUT
+{
+    float4 pos : SV_POSITION; //位置
+    float2 uv : TEXCOORD; //UV座標
+    float4 color : COLOR; //色（明るさ）
+    float4 normal : NORMAL;
+};
+
+//───────────────────────────────────────
+// 頂点シェーダ
+//───────────────────────────────────────
+float4 VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL) : SV_Position
+{
+	//ピクセルシェーダーへ渡す情報
+    float4 outPos;
+    
+    normal.w = 0;
+    normal = normalize(normal);
+    pos = pos + normal * 0.05;
+    
+    pos = mul(pos, matWVP);
+    
+    return pos;
+}
+
+//───────────────────────────────────────
+// ピクセルシェーダ
+//───────────────────────────────────────
+float4 PS(VS_OUT inData) : SV_Target
+{
+    return float4(0, 0, 0, 1.0);
+}
